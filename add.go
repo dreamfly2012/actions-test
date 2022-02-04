804c07ce0e8eec74ee79b3f71b505b38fbd348aa
menghuiguli <fujia0@qq.com>
2022-02-04 20:55:31 +0800

    add sub function

--- add.go
+++ add.go
@@ -3,3 +3,7 @@ package main
 func Add(a int, b int) int {
 	return a + b
 }
+
+func Sub(a int, b int) int {
+	return a - b
+}
--- add_test.go
+++ add_test.go
@@ -1,6 +1,8 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestAdd(t *testing.T) {
 	type args struct {
@@ -13,18 +15,54 @@ func TestAdd(t *testing.T) {
 		want int
 	}{
 		{
-			name: "",
+			name: "add_one",
 			args: args{
-				a: 0,
+				a: 2,
 				b: 0,
 			},
-			want: 0,
+			want: 2,
+		},
+		{
+			name: "add_two",
+			args: args{
+				a: 2,
+				b: -4,
+			},
+			want: -3,
 		},
 	}
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			if got := Add(tt.args.a, tt.args.b); got != tt.want {
 				t.Errorf("Add() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestSub(t *testing.T) {
+	type args struct {
+		a int
+		b int
+	}
+	tests := []struct {
+		name string
+		args args
+		want int
+	}{
+		{
+			name: "test_sub",
+			args: args{
+				a: 0,
+				b: 9,
+			},
+			want: -9,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			if got := Sub(tt.args.a, tt.args.b); got != tt.want {
+				t.Errorf("Sub() = %v, want %v", got, tt.want)
 			}
 		})
 	}
